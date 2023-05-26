c4f5dc87297096e2e0d85d7c1264cd568de92f8e
Harout Babikian <hhbabikian@gmail.com>
2023-05-26 20:26:47 +0300

    Draft commit

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -80,5 +81,49 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         //
+    }
+
+    public function calculateClicks($period)
+    {
+        $validPeriods = ['last_24_hours', 'last_week', 'last_month', 'last_year'];
+
+        if (!in_array($period, $validPeriods)) {
+            return response()->json(['message' => 'Invalid period'], 400);
+        }
+
+        $clicks = Category::select(
+            'name',
+            DB::raw('SUM(click_count) AS total_clicks'),
+            DB::raw('COUNT(DISTINCT user_id) AS unique_clicks')
+        )
+        ->whereBetween('created_at', $this->getPeriodRange($period))
+        ->groupBy('name')
+        ->get();
+
+        return response()->json(['clicks' => $clicks]);
+    }
+
+    /**
+     * Get the date range for the specified period.
+     *
+     * @param string $period
+     * @return array
+     */
+    private function getPeriodRange($period)
+    {
+        $now = now();
+
+        switch ($period) {
+            case 'last_24_hours':
+                return [$now->copy()->subDay(), $now];
+            case 'last_week':
+                return [$now->copy()->subWeek(), $now];
+            case 'last_month':
+                return [$now->copy()->subMonth(), $now];
+            case 'last_year':
+                return [$now->copy()->subYear(), $now];
+        }
+
+        return [];
     }
 }
